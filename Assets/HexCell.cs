b7aef38764cf57a486374a4624b0db97affed848
redbearsam <redbear.sam@outlook.com>
2017-01-08 17:57:25 +0000

    Initial commit

---
+++ Assets/HexCell.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour {
+	public HexCoordinates coordinates;
+	public Color color;
+	public float height;
+	Mesh collisionMesh;
+	private List<Vector3> localvertices;
+	private List<int> localtriangles;
+	private List<Color> localcolors;
+
+	public void Awake()
+	{
+		GetComponent<MeshFilter>().mesh = collisionMesh = new Mesh();
+		localvertices = new List<Vector3>();
+		localtriangles = new List<int>();
+		localcolors = new List<Color>();
+	}
+	public void makeMesh()
+	{
+		collisionMesh.Clear();
+		localvertices.Clear();
+		localtriangles.Clear();
+		localcolors.Clear();
+		Vector3 center = new Vector3(0f, height, 0f);
+		for(int i = 0; i < 6; i++)
+		{
+			Vector3 topCorner1 = center + HexMetrics.corners[i];
+			Vector3 topCorner2 = center + HexMetrics.corners[i + 1];
+			AddTriangle(center, topCorner1, topCorner2);
+			Vector3 bottomCorner1 = new Vector3(topCorner1.x, 0f, topCorner1.z);
+			Vector3 bottomCorner2 = new Vector3(topCorner2.x, 0f, topCorner2.z);
+			AddTriangle(topCorner2, topCorner1, bottomCorner1);
+			AddTriangle(bottomCorner2, topCorner2, bottomCorner1);
+		}
+		collisionMesh.vertices = localvertices.ToArray();
+		collisionMesh.triangles = localtriangles.ToArray();
+		collisionMesh.RecalculateNormals();
+		collisionMesh.colors = localcolors.ToArray();
+	}
+
+	void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+	{
+		int vertexIndex = localvertices.Count;
+		localvertices.Add(v1);
+		localvertices.Add(v2);
+		localvertices.Add(v3);
+		localtriangles.Add(vertexIndex);
+		localtriangles.Add(vertexIndex + 1);
+		localtriangles.Add(vertexIndex + 2);
+	}
+}
---
+++ Assets/HexMesh.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour
+{
+
+	List<Mesh> collisionMeshes;
+	List<HexMesh> hexMeshes;
+	List<Vector3> vertices;
+	List<int> triangles;
+	List<MeshCollider> meshColliders;
+	List<Color> colors;
+
+	void Awake()
+	{
+		hexMeshes = new List<HexMesh>();
+		collisionMeshes = new List<Mesh>();
+		meshColliders = new List<MeshCollider>();//gameObject.AddComponent<MeshCollider>();
+		vertices = new List<Vector3>();
+		triangles = new List<int>();
+		colors = new List<Color>();
+	}
+	public void Triangulate(HexCell[] cells)
+	{
+		foreach(MeshCollider meshCollider in meshColliders)
+		{
+			MeshCollider.Destroy(meshCollider);
+		}
+		collisionMeshes.Clear();
+		meshColliders.Clear();
+		for (int i = 0; i < cells.Length / 100; i++) {
+			Mesh mesh = new Mesh();
+			GetComponent<MeshFilter>().mesh = mesh;
+			vertices.Clear();
+			triangles.Clear();
+			colors.Clear();
+			int startPosition = i * 100;
+			int endPosition = (i + 1) * 100;
+
+			for (int j = startPosition; j < endPosition; j++)
+			{
+				Triangulate(cells[j]);
+			}
+
+			mesh.vertices = vertices.ToArray();
+			mesh.triangles = triangles.ToArray();
+			mesh.RecalculateNormals();
+			mesh.colors = colors.ToArray();
+			MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+			meshCollider.sharedMesh = mesh;
+			collisionMeshes.Add(mesh);
+			meshColliders.Add(meshCollider);
+		}
+	}
+
+	void Triangulate(HexCell cell)
+	{
+		Vector3 center = cell.transform.localPosition;
+		Vector3 centerWithHeight = new Vector3(center.x, cell.height, center.z);
+		for (int i = 0; i < 6; i++)
+		{
+			AddTriangle(centerWithHeight, centerWithHeight + HexMetrics.corners[i], centerWithHeight + HexMetrics.corners[i+1]);
+		}
+		cell.makeMesh();
+	}
+
+	void AddTriangleColor(Color color)
+	{
+		colors.Add(color);
+		colors.Add(color);
+		colors.Add(color);
+	}
+	void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+	{
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+	}
+}
